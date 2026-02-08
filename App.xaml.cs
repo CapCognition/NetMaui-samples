0f869dacc2e3ad4bff601befca11a79b3cafca90
CapCognitionNicola <nicola@capcognition.com>
2026-02-08 15:56:30 +0100

    SDK upgrade, small UI fixes

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,8 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
+
+        UserAppTheme = AppTheme.Dark;
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
